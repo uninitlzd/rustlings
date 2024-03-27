4a279266cad2c5e0894e7355544403d76ae0d833
Alfredo Eka Wibowo <edo@mac-mini-m1.local>
2024-03-28 02:58:34 +0700

    progress: primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: u32) -> u32 {
+    return if (x > 40) { x * 1 } else { x * 2} ;
+}
 
 // Don't modify this function!
 #[test]
